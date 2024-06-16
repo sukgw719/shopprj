6a33c2e218ce5e018cee0a21854cf125f6d32a03
sukgw719 <sukgw719@naver.com>
2024-06-16 15:54:39 +0900

    first commit

---
+++ src/test/java/prj/shopmall/shopmall/ShopmallApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.shopmall.shopmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
